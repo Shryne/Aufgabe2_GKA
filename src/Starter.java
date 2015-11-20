6f255a439e6df9fd5ae29e1cff10598a3cf360e7
Narow <enomeral@gmx.de>
2015-11-20 12:21:49 +0100

    Initial commit

---
+++ src/Starter.java
@@ -0,0 +1,11 @@
+import ADTGraph.Vertex;
+
+/**
+ * Created by remen on 20.11.15.
+ */
+public class Starter {
+
+    public static void main(String args[]) {
+        Vertex v = Vertex.createV("A");
+    }
+}
